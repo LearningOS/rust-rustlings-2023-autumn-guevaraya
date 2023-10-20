1f51efef0a94cd8419561cdc0b5e8da15df8c47c
guuevara.ya <guevara.ya@gmail.com>
2023-10-20 11:55:29 +0800

    done

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty(){
+            Err(ParsePersonError::Empty)
+        }else{
+
+            let (name,age_str) = s.split_once(',').ok_or(ParsePersonError::BadLen)?;
+            if name.is_empty() {
+                Err(ParsePersonError::NoName)
+            }else{
+                if age_str.contains(","){ 
+                    return Err(ParsePersonError::BadLen)
+                }
+                let age = age_str.parse::<usize>().map_err(|error|ParsePersonError::ParseInt(error))?;
+                Ok(Person{name:String::from(name),age:age})
+            }
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,11 +27,10 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
-// integers, an array of three integers, and a slice of integers.
+// integers, an sliceay of three integers, and a slice of integers.
 //
 // Note that the implementation for tuple and array will be checked at compile
 // time, but the slice implementation needs to check the slice length! Also note
@@ -41,6 +40,21 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        //let vec:Vec<i16> = tuple.into();
+        /* 
+         * if  vec.len() != 3 {
+            Err(Self::Error::BadLen)
+        }else
+        */
+        if tuple.0 > 0 &&  tuple.1 > 0 &&  tuple.2 > 0 && 
+            tuple.0 <= 256 && tuple.1 <= 256 && tuple.2 <= 256  {
+            Ok(Color{red:tuple.0 as u8,
+                green:tuple.1 as u8,
+                blue:tuple.2 as u8})
+        }else{
+            Err(Self::Error::IntConversion)
+        }
+        
     }
 }
 
@@ -48,6 +62,15 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if arr[0] > 0 &&  arr[1] > 0 &&  arr[2] > 0 && 
+            arr[0] <= 256 && arr[1] <= 256 && arr[2] <= 256  {
+            Ok(Color{red:arr[0] as u8,
+                green:arr[1] as u8,
+                blue:arr[2] as u8})
+        }else{
+            Err(Self::Error::IntConversion)
+        }
+
     }
 }
 
@@ -55,6 +78,19 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3{
+            Err(Self::Error::BadLen)
+        }else
+        if slice[0] > 0 &&  slice[1] > 0 &&  slice[2] > 0 && 
+            slice[0] <= 256 && slice[1] <= 256 && slice[2] <= 256  {
+            Ok(Color{red:slice[0] as u8,
+                green:slice[1] as u8,
+                blue:slice[2] as u8})
+        }else{
+            Err(Self::Error::IntConversion)
+        }
+
+
     }
 }
 
